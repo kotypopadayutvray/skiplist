ea5f1d036a4de28906753169f156ff0205f38324
Maxim Golikov <blikylia@gmail.com>
2018-11-26 21:30:11 +0300

    Add functions for search and delete in SkipList structure

--- lib/skip_list/skip_list.go
+++ lib/skip_list/skip_list.go
@@ -24,7 +24,7 @@ func NewSkipList(maxLevel int, prob float64) *SkipList {
 	}
 }
 
-func (sl *SkipList) GenerateLevel() int {
+func (sl *SkipList) generateLevel() int {
 	level := 1
 	for rand.Float64() < sl.Probability && level <= sl.MaxLevel {
 		level++
@@ -35,7 +35,7 @@ func (sl *SkipList) GenerateLevel() int {
 	return level
 }
 
-func (sl *SkipList) Insert(key int) {
+func (sl *SkipList) path(key int) [] *node.Node {
 	// Create an update slice
 	// Elements of the update will contain pointers to one element from
 	// any of all levels. It necessary for inserting element in the future
@@ -57,13 +57,48 @@ func (sl *SkipList) Insert(key int) {
 		// Insert found node in the update array
 		update[level] = current_node
 	}
+	return update
+}
+
+func (sl *SkipList) Search(key int) bool {
+	path := sl.path(key)
+	low_level := 0
+	return path[low_level].Forward[low_level].Key == key
+}
+
+func (sl *SkipList) Insert(key int) {
+	// Create the path array
+	path := sl.path(key)
+	// Check if we have the same key
+	// For this purpose get the low level element
+	low_level_element := path[0].Forward[0]
+	// And compare its key with inserted key
+	if low_level_element != nil && low_level_element.Key == key {
+		// If keys are equal don't insert
+		return
+	}
 	// Get the new level of the new element
-	new_element_level := sl.GenerateLevel()
+	new_element_level := sl.generateLevel()
 	// Create new element
 	new_element := node.NewNode(key, new_element_level)
 	// Insert new element in the list
 	for i := 0; i < new_element_level; i++ {
-		new_element.Forward[i] = update[i].Forward[i]
-		update[i].Forward[i] = new_element
+		// Update pointer for every level
+		new_element.Forward[i] = path[i].Forward[i]
+		path[i].Forward[i] = new_element
+	}
+}
+
+func (sl *SkipList) Delete(key int) {
+	path := sl.path(key)
+	low_level := 0
+	deleting_element := path[low_level].Forward[low_level]
+	if deleting_element.Key == key {
+		for level := 0; level < len(path); level++ {
+			if path[level].Forward[level] != deleting_element {
+				continue
+			}
+			path[level].Forward[level] = deleting_element.Forward[level]
+		}
 	}
 }

6aeda058d76877d87691f91bb7353b78daa0ca6a
Maxim Golikov <blikylia@gmail.com>
2018-11-29 00:16:17 +0300

    change code style

--- lib/node/node.go
+++ lib/node/node.go
@@ -1,19 +1,19 @@
 package node
 
 type Node struct {
-	Key int
-	Forward [] *Node		// Slice
+	Key     int
+	Forward []*Node // Slice
 }
 
 func NewNode(key, level int) *Node {
 	// Create the empty slice
-	forward := [] *Node { }
+	forward := []*Node{}
 	// Fill the slice by the nil values
 	for i := 0; i < level; i++ {
 		forward = append(forward, nil)
 	}
-	return &Node {
-		Key: key,
+	return &Node{
+		Key:     key,
 		Forward: forward,
 	}
 }
--- lib/skip_list/skip_list.go
+++ lib/skip_list/skip_list.go
@@ -1,27 +1,31 @@
-package skip_list
+package skiplist
 
 import (
-	"../node"
 	"math/rand"
+
+	"../node"
 )
 
+// SkipList structure
 type SkipList struct {
-	MaxLevel int
-	Header *node.Node
+	MaxLevel    int
+	Header      *node.Node
 	Probability float64
 }
 
+// NewSkipList function create a new SkipList instance
 func NewSkipList(maxLevel int, prob float64) *SkipList {
 	// Create the header node
 	// Header will contain pointers to inserted nodes
 	header := node.NewNode(0, maxLevel)
-	return &SkipList {
-		MaxLevel: maxLevel,
-		Header: header,
+	return &SkipList{
+		MaxLevel:    maxLevel,
+		Header:      header,
 		Probability: prob,
 	}
 }
 
+// Generate level of the new inserted element
 func (sl *SkipList) generateLevel() int {
 	level := 1
 	for rand.Float64() < sl.Probability && level <= sl.MaxLevel {
@@ -33,70 +37,76 @@ func (sl *SkipList) generateLevel() int {
 	return level
 }
 
-func (sl *SkipList) path(key int) [] *node.Node {
+func (sl *SkipList) path(key int) []*node.Node {
 	// Create an update slice
 	// Elements of the update will contain pointers to one element from
 	// any of all levels. It necessary for inserting element in the future
-	update := [] *node.Node { }
+	update := []*node.Node{}
 	// Fill update array by nul values
 	for i := 0; i < sl.MaxLevel; i++ {
 		update = append(update, nil)
 	}
 	// Start search place for inserting from header node
-	current_node := sl.Header
+	currentNode := sl.Header
 	// Go from the highest level
 	for level := sl.MaxLevel - 1; level >= 0; level-- {
 		// If current node has next element in the same level
 		// and this next element has key less than inserted key
-		for current_node.Forward[level] != nil && current_node.Forward[level].Key < key {
+		for currentNode.Forward[level] != nil && currentNode.Forward[level].Key < key {
 			// Move through the current level next
-			current_node = current_node.Forward[level]
+			currentNode = currentNode.Forward[level]
 		}
 		// Insert found node in the update array
-		update[level] = current_node
+		update[level] = currentNode
 	}
 	return update
 }
 
+// Search element by key in the SkipList instance
 func (sl *SkipList) Search(key int) bool {
 	path := sl.path(key)
-	low_level := 0
-	return path[low_level].Forward[low_level].Key == key
+	lowLevel := 0
+	return path[lowLevel].Forward[lowLevel].Key == key
 }
 
-func (sl *SkipList) Insert(key int) {
+// Insert the new element with key in the SkipList instance
+func (sl *SkipList) Insert(key int) bool {
 	// Create the path array
 	path := sl.path(key)
 	// Check if we have the same key
 	// For this purpose get the low level element
-	low_level_element := path[0].Forward[0]
+	lowLevelElement := path[0].Forward[0]
 	// And compare its key with inserted key
-	if low_level_element != nil && low_level_element.Key == key {
+	if lowLevelElement != nil && lowLevelElement.Key == key {
 		// If keys are equal don't insert
-		return
+		return false
 	}
 	// Get the new level of the new element
-	new_element_level := sl.generateLevel()
+	newElementLevel := sl.generateLevel()
 	// Create new element
-	new_element := node.NewNode(key, new_element_level)
+	newElement := node.NewNode(key, newElementLevel)
 	// Insert new element in the list
-	for i := 0; i < new_element_level; i++ {
+	for i := 0; i < newElementLevel; i++ {
 		// Update pointer for every level
-		new_element.Forward[i] = path[i].Forward[i]
-		path[i].Forward[i] = new_element
+		newElement.Forward[i] = path[i].Forward[i]
+		path[i].Forward[i] = newElement
 	}
+	return true
 }
 
-func (sl *SkipList) Delete(key int) {
+// Delete element in the SkipList instance
+func (sl *SkipList) Delete(key int) bool {
 	path := sl.path(key)
-	low_level := 0
-	deleting_element := path[low_level].Forward[low_level]
-	if deleting_element.Key == key {
+	lowLevel := 0
+	deletingElement := path[lowLevel].Forward[lowLevel]
+	if deletingElement != nil && deletingElement.Key == key {
 		for level := 0; level < len(path); level++ {
-			if path[level].Forward[level] != deleting_element {
+			if path[level].Forward[level] != deletingElement {
 				continue
 			}
-			path[level].Forward[level] = deleting_element.Forward[level]
+			path[level].Forward[level] = deletingElement.Forward[level]
 		}
+		return true
 	}
+	return false
 }
